8aa943c724e294abfaa4e7871644200af3479986
ManuelMares <fmms@outlook.com>
2023-02-20 08:42:54 -0700

    fixed tailwind

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import './App.css';
 import React from 'react'
 import {Stuff} from './components/Stuff'
 import {NewStuff} from './components/NewStuff'
-
+import Example from './components/Example'
+import Example2 from './components/Example2'
 
 function App() {
   return (
     <div className="App" >
       <NewStuff/>
+      <Example/>
+      <Example2/>
+
+
     </div>
 
   );
