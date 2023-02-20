16e497100ce3cc47bebd88a4d260b333b0f0a47b
ManuelMares <fmms@outlook.com>
2023-02-20 09:23:31 -0700

    fixed npm

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ import {Stuff} from './components/Stuff'
 import {NewStuff} from './components/NewStuff'
 import Example from './components/Example'
 import Example2 from './components/Example2'
+import Example3 from './components/Example3'
 
 function App() {
   return (
     <div className="App" >
-      <NewStuff/>
+      {/* <NewStuff/> */}
       <Example/>
       <Example2/>
+      <Example3/>
 
 
     </div>
